bbd7194c3d7824ab76b2f809aaa9134ade2b4966
xzc1993@gmail.com <xxx@xxx.xxx>
2016-12-10 16:23:33 +0100

    Deployment test. #4

--- codeDeploy_worker_scripts/ApplicationStart.sh
+++ codeDeploy_worker_scripts/ApplicationStart.sh
@@ -1,3 +1,3 @@
 export C_FORCE_ROOT='true'
 cd /home/ubuntu/iosr
-sudo celery -A tasks worker --loglevel=info --pool=solo > /home/ubuntu/iosr/launch.log 2>&1 &
+celery -A tasks worker --loglevel=info --pool=solo > /home/ubuntu/iosr/launch.log 2>&1 &
