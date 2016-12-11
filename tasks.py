fb6678549923600163c140265f1c6b54f4a85390
xzc1993@gmail.com <xxx@xxx.xxx>
2016-12-12 00:18:08 +0100

    Added 3rd mongo replica server

--- tasks.py
+++ tasks.py
@@ -22,17 +22,3 @@ def add(x, y):
         'timestamp' : datetime.utcnow()
     })
     return x + y + 1000
-
-
-client = MongoClient([
-    'ec2-54-154-102-56.eu-west-1.compute.amazonaws.com',
-    'ec2-54-171-39-125.eu-west-1.compute.amazonaws.com',
-    'ec2-54-154-102-29.eu-west-1.compute.amazonaws.com']
-)
-db = client.primer
-coll = db.log
-coll.insert_one({
-    "a": 1,
-    "b": 2,
-    'timestamp': datetime.utcnow()
-})
