f9cb1df119e144bbb0f62f4d47d8eca5f3a41230
xzc1993@gmail.com <xxx@xxx.xxx>
2016-12-11 19:14:25 +0100

    MQTT and Mongo LB test

--- tasks.py
+++ tasks.py
@@ -1,9 +1,22 @@
 from celery import Celery
+from pymongo import MongoClient
+from datetime import datetime
 
-app = Celery('tasks', backend='amqp://kotek:kotek@ec2-54-154-186-239.eu-west-1.compute.amazonaws.com//', broker='amqp://kotek:kotek@ec2-54-154-186-239.eu-west-1.compute.amazonaws.com//')
+app = Celery('tasks',
+             backend='amqp://kotek:kotek@rabbitmq-1321755515.eu-west-1.elb.amazonaws.com//',
+             broker='amqp://kotek:kotek@rabbitmq-1321755515.eu-west-1.elb.amazonaws.com//'
+             )
 
 @app.task
 def add(x, y):
+    client = MongoClient("mongodb://mongo-1541864808.eu-west-1.elb.amazonaws.com:27017")
+    db = client.primer
+    coll = db.log
+    coll.insert_one({
+        "a" : x,
+        "b" : y,
+        'timestamp' : datetime.utcnow()
+    })
     return x + y + 1000
 
 
