731e99eabbb5a16795a33b991cf5ff5ea9ae152f
xzc1993@gmail.com <xxx@xxx.xxx>
2016-12-12 00:17:52 +0100

    Added 3rd mongo replica server

--- tasks.py
+++ tasks.py
@@ -11,7 +11,8 @@ app = Celery('tasks',
 def add(x, y):
     client = MongoClient([
         'ec2-54-154-102-56.eu-west-1.compute.amazonaws.com',
-        'ec2-54-154-102-56.eu-west-1.compute.amazonaws.com']
+        'ec2-54-171-39-125.eu-west-1.compute.amazonaws.com',
+        'ec2-54-154-102-29.eu-west-1.compute.amazonaws.com']
     )
     db = client.primer
     coll = db.log
@@ -21,3 +22,17 @@ def add(x, y):
         'timestamp' : datetime.utcnow()
     })
     return x + y + 1000
+
+
+client = MongoClient([
+    'ec2-54-154-102-56.eu-west-1.compute.amazonaws.com',
+    'ec2-54-171-39-125.eu-west-1.compute.amazonaws.com',
+    'ec2-54-154-102-29.eu-west-1.compute.amazonaws.com']
+)
+db = client.primer
+coll = db.log
+coll.insert_one({
+    "a": 1,
+    "b": 2,
+    'timestamp': datetime.utcnow()
+})
