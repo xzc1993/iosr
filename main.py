99a949455e5e7a610571c0b9b6546ca4cdfcf215
xzc1993@gmail.com <xxx@xxx.xxx>
2017-01-06 23:18:30 +0100

    Added image processing capability. Added user login logout feature.

--- main.py
+++ main.py
@@ -1,29 +1,64 @@
 import os
-
+import StringIO
 import tornado.ioloop
 import tornado.web
 import tornado.gen
 import tasks
 
-class MainHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class LoginHandler(BaseHandler):
 
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
-        self.render( os.path.join( os.path.dirname(__file__), 'templates/index.html'))
+        self.render( os.path.join( os.path.dirname(__file__), 'templates/login.html'))
 
     def post(self):
-        print 'Executing task'
-        result = tasks.add.apply_async([int(self.get_argument("a")), int(self.get_argument("b"))])
-        message = "Hello world and sum is {}".format(result.get(timeout=10))
-        print (message)
-        self.write(message)
+        if( self.get_argument("username")[0] == 'k' and self.get_argument("password")[0] == 'k'):
+            self.set_secure_cookie('user', self.get_argument("username"))
+            self.redirect(self.get_argument("next", "/"))
+        else:
+            raise tornado.web.HTTPError(403)
+
+class LogoutHandler(BaseHandler):
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    @tornado.web.authenticated
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect("/login")
+
+class MainHandler(BaseHandler):
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    @tornado.web.authenticated
+    def get(self):
+        self.render( os.path.join( os.path.dirname(__file__), 'templates/index.html'))
+
+    @tornado.web.authenticated
+    def post(self):
+        fileinfo = self.request.files['filearg'][0]
+        self.write(tasks.blur(fileinfo['body']))
+        self.add_header('Content-Type', 'image/png')
         self.finish()
 
 def make_app():
+    settings = {
+        "static_path": os.path.join(os.path.dirname(__file__), "static"),
+        "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        "login_url": "/login",
+        "xsrf_cookies": True,
+    }
     return tornado.web.Application([
         (r"/", MainHandler),
-    ])
+        (r"/login", LoginHandler),
+        (r"/logout", LogoutHandler),
+    ], **settings)
 
 if __name__ == "__main__":
     app = make_app()
--- tasks.py
+++ tasks.py
@@ -1,24 +1,78 @@
+import datetime
+import StringIO
+
 from celery import Celery
+from PIL import Image
+from PIL import ImageFilter
 from pymongo import MongoClient
-from datetime import datetime
 
 app = Celery('tasks',
-             backend='amqp://kotek:kotek@rabbitmq-1321755515.eu-west-1.elb.amazonaws.com//',
-             broker='amqp://kotek:kotek@rabbitmq-1321755515.eu-west-1.elb.amazonaws.com//'
-             )
+     backend='amqp://kotek:kotek@rabbitmq-1321755515.eu-west-1.elb.amazonaws.com//',
+     broker='amqp://kotek:kotek@rabbitmq-1321755515.eu-west-1.elb.amazonaws.com//'
+)
+
+client = MongoClient(
+    # 'ec2-54-154-102-56.eu-west-1.compute.amazonaws.com',
+    # 'ec2-54-171-39-125.eu-west-1.compute.amazonaws.com',
+    # 'ec2-54-154-102-29.eu-west-1.compute.amazonaws.com']
+)
+
 
 @app.task
 def add(x, y):
-    client = MongoClient([
-        'ec2-54-154-102-56.eu-west-1.compute.amazonaws.com',
-        'ec2-54-171-39-125.eu-west-1.compute.amazonaws.com',
-        'ec2-54-154-102-29.eu-west-1.compute.amazonaws.com']
-    )
     db = client.primer
     coll = db.log
     coll.insert_one({
         "a" : x,
         "b" : y,
-        'timestamp' : datetime.utcnow()
+        'timestamp' : datetime.datetime.utcnow()
     })
     return x + y + 1000
+
+
+def remoteImageConverter(fn):
+    def wrappedConverter(imageData, *args, **kwars):
+        buff = StringIO.StringIO(imageData)
+        buff.seek(0)
+        image = Image.open(buff)
+        convertedImage = fn(image, *args, **kwars)
+        buff = StringIO.StringIO()
+        convertedImage.save(buff, format="PNG")
+        outputImageData = buff.getvalue()
+        buff.close()
+        logToDatabase(fn.__name__)
+        return outputImageData
+    return wrappedConverter
+
+
+def logToDatabase(operationName):
+    db = client.iosr
+    coll = db.log
+    coll.insert_one({
+        'operationName': operationName,
+        'timestamp': datetime.datetime.utcnow(),
+    })
+
+@app.task
+def takeCPU(time):
+    start = time.time()
+    while start + time > time.time():
+        x = (1+3)/5
+    return x
+
+
+@app.task
+@remoteImageConverter
+def convertToGreyscale(image):
+    return image.convert('L')
+
+
+@app.task
+@remoteImageConverter
+def blur(image):
+    return image.filter(ImageFilter.BLUR)
+
+
+
+
+
